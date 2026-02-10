f537735542868850084228773bd11fc47f85a101
Heebac25 <h89611392@gmail.com>
2026-02-10 19:20:06 +0000

    Finished learning about frames ; going to implement them into my task_app

--- frame.py
+++ frame.py
@@ -7,12 +7,12 @@ font = Font(family="Arial", size=30)
 screen.title("Frame Organisation")
 
 # Frame Definitions
-frame1 = Frame(screen, bg="#000000", width=10, height=5, relief="groove", bd=5)
-frame1.grid(column=10,row=10)
+frame1 = Frame(screen, bg="#000000", width=10, height=5, relief="groove", bd=5, cursor="hand2")
+frame1.pack(padx=50, pady=10)
 
 # Label Definitions
 label1 = Label(frame1, text="This is a frame! 🤪", font=font)
-label1.grid(column=5, row=5)
+label1.pack()
 
 screen.mainloop()
 
--- task_app.py
+++ task_app.py
@@ -6,11 +6,11 @@ screen = Tk()
 # Different function definitions for processes
 def clicking():
     user_result = f"Your task name is: {user_input.get()}"
-    text_label.configure(text = user_result)
+    hidden_label.configure(text = user_result)
 
 
 screen.title("Task app")
-text = "hello world"
+text = "Enter task here please"
 font = Font(family="Arial", size=30)
 button_font = Font(family="Arial", size=20)
 
@@ -18,7 +18,9 @@ hello = "hello mate"
 
 # Label definitions
 text_label = Label(screen, text=text, font=font)
-
+text_label.grid(column=1, row=2)
+hidden_label = Label(screen, text="", font=font)
+hidden_label.grid(column=3, row=6)
 # Creating the menu 
 
 # Viewbar settings
@@ -38,17 +40,14 @@ view_bar.add_cascade(label='Leave application',menu=leaving_now)
 screen.config(menu=view_bar)
 
 # Creating Entry field
-user_input = Entry(screen, width=15)
-user_input.grid(column=1,row=2)
-
-
+user_input = Entry(screen, width=10, cursor="xterm")
+user_input.grid(column=2,row=2)
 
 # Button Definitions
-button1 = Button(screen, text="Click me please 🤓", fg="blue",command=clicking, width=18, height=6, font=button_font)
+button1 = Button(screen, text="Click me please 🤓", fg="blue",command=clicking, width=15, height=6, font=button_font)
 
 # Outputting labels onto screen
 button1.grid(column=3, row=2)
-text_label.grid(column=2, row=2)
 
 
 # Executing entire program
